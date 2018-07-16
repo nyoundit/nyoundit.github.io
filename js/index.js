7af8205104e04f55d83b5b5a6b6060ff0972a05b
nyoundit <nyoundit.m@gmail.com>
2018-07-16 17:12:59 +0300

    made html and css files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var canvas = document.querySelector('canvas');
+
+// resizing the canvas element
+canvas.innerHeight = window.innerHeight;
+canvas.innerWidth = window.innerWidth;
+
+console.log(canvas);
