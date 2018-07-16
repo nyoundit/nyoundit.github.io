253d1609741ebfe9d39c8787a819bc090f6cece3
nyoundit <nyoundit.m@gmail.com>
2018-07-16 17:23:28 +0300

    removed canvas element and styling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,2 @@
-var canvas = document.querySelector('canvas');
 
-// resizing the canvas element
-canvas.innerHeight = window.innerHeight;
-canvas.innerWidth = window.innerWidth;
-
-console.log(canvas);
+console.log('this the beginning');
